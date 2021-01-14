38eb628537b922aee2dd1a0eccc51f9f8e31dced
Yuri Miranda Fialho <yuri@crmpiperun.com>
2021-01-14 14:22:21 -0300

    Instalação do Analytics

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,15 @@ export default function Home({ allPostsData }) {
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-L7CBVL47XG"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-L7CBVL47XG');
+        </script>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
