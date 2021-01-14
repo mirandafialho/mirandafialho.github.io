0cdf20ebb90ae80dbe128ec152b66162ce095ecb
Yuri Miranda Fialho <yuri@crmpiperun.com>
2021-01-14 14:08:08 -0300

    Adding WhatsApp Form

--- pages/index.js
+++ pages/index.js
@@ -46,6 +46,8 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
+      <script type="text/javascript">window.piperun_data = {"form":{"header":{"text":"Quero Entrar Em Contato","background_color":"#075E54","color":"#FFFFFF"},"content":{"fields":[{"field":"name","type":"Pessoa","name":"Nome","id":"item_17","required":false},{"field":"cpf","type":"Pessoa","name":"CPF","id":"item_24","required":false},{"field":"email","type":"Pessoa","name":"E-mail","id":"item_18","required":false},{"field":"mobile_phone","type":"Pessoa","name":"Telefone","id":"item_19","required":false},{"field":"notes","type":"Oportunidade","name":"Mensagem","id":"item_0","required":false}],"background_color":"#ECE5DD","color":"#2F2F2F"},"button":{"text":"Salvar","background_color":"#5CB85C","color":"#FFFFFF"},"accept":{"text":" Concordo em receber comunicações de ofertas personalizadas de acordo com meus interesses.","color":"#2F2F2F"},"logo":{"color":"#25D366"}},"enterprise":"false","hash":"00c3f6ae-b5ea-4d5b-bcc1-9eac73d5d506","contact_phone":"55991637123","success_page_url":"","google_analytics":""};</script>
+      <script src="https://crmpiperun.github.io/whatsapp-gerador-lead/script.js" type="text/javascript"></script>
     </Layout>
   )
 }
